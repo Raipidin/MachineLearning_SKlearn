c44b9201d1878e8eb3e8a4b78660c809f1ed8ef5
Marcos <goncmarcos05@gmail.com>
2023-09-11 16:20:36 -0300

    a

--- SKlearn_T1.py
+++ SKlearn_T1.py
@@ -1,4 +1,5 @@
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
 #features 
 # 1 bico
@@ -14,12 +15,39 @@ passaro3 = [0, 1, 1]
 
 # 0 - passaro
 # 1 - galinha
-dados = [galinha1, galinha2, galinha3, passaro1, passaro2, passaro3]
-classes = [1, 1, 1, 0, 0, 0]
+train_x = [galinha1, galinha2, galinha3, passaro1, passaro2, passaro3]
+train_y = [1, 1, 1, 0, 0, 0]
 
 model = LinearSVC()
-model.fit(dados, classes)
+model.fit(train_x, train_y)
 
 animal_secreto = [1, 1, 1]
 
-model.predict([animal_secreto])
+print(model.predict([animal_secreto]))
+
+segredo1 = [1, 1, 1]
+segredo2 = [1, 1, 0]
+segredo3 = [0, 1, 1]
+
+test_x = [segredo1, segredo2, segredo3]
+test_y = [1, 0, 0]
+
+previsoes = model.predict(test_x)
+
+print(previsoes)
+
+corretos = (previsoes == test_y).sum()
+
+print(corretos) 
+
+total = len(test_x)
+
+taxa_de_acerto = corretos / total
+
+#sem o accuraçy score
+print('Taxa de acerto: %.2f' %(taxa_de_acerto * 100), '%')
+
+#no accuraçy score o primeiro parametro é o valor real e o segundo é o valor previsto
+accuracy = accuracy_score(test_y, previsoes)
+
+print('Accuracy: %.2f' %(accuracy * 100), '%')
